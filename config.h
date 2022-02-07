588730baadaeb105969a745cc6ef896ca76ffe19
Biosplat <>
2022-02-06 19:43:32 -0500

    upodate

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xf0;
+static const unsigned int alpha = 0xff;
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -33,4 +33,4 @@ static unsigned int lines      = 16;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 2;
