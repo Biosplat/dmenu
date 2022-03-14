7a9a592885cbfb3b0e714d7d48a5be73b63e0b77
Biosplat <>
2022-03-13 21:50:15 -0400

    onedark

--- config.h
+++ config.h
@@ -7,15 +7,11 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Fira Code:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel]  = { "#eeeeee", "#005577" },
-	[SchemeOut]  = { "#000000", "#00ffff" },
-};
+
+#include "onedark.h"
 
 static const unsigned int alphas[SchemeLast][2] = {
 	[SchemeNorm] = { OPAQUE, alpha },
@@ -33,4 +29,4 @@ static unsigned int lines      = 16;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 2;
+static const unsigned int border_width = 3;
---
+++ onedark.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ABB2BF", "#1E2127" },
+	[SchemeSel] = { "#ABB2BF", "#303640" },
+	[SchemeOut] = { "#61AFEF", "#E06C75" },
+};
