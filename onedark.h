8391d7780849ffd94a1cb600651ccb524a34ccf7
Biosplat <>
2022-05-02 20:12:03 -0400

    new scheme

--- onedark.h
+++ onedark.h
@@ -1,6 +1,19 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ABB2BF", "#1E2127" },
-	[SchemeSel] = { "#ABB2BF", "#303640" },
-	[SchemeOut] = { "#61AFEF", "#E06C75" },
+
+	// One Dark
+	// [SchemeNorm] = { "#ABB2BF", "#1E2127" },
+	// [SchemeSel] =  { "#ABB2BF", "#303640" },
+	// [SchemeOut] =  { "#61AFEF", "#E06C75" },
+
+	// Tokyo Night
+	// [SchemeNorm] = { "#ACB0D0", "#1A1B26" },
+	// [SchemeSel] =  { "#ACB0D0", "#4E5173" },
+	// [SchemeOut] =  { "#7AA2F7", "#F7768E" },
+	
+	// Catppuccin
+	[SchemeNorm] = { "#D9E0EE", "#1E1E2E" },
+	[SchemeSel] =  { "#1E1E2E", "#96CDFB" }, // MAYBE C3BAC6
+	[SchemeOut] =  { "#96CDFB", "#F28FAD" },
+
 };
